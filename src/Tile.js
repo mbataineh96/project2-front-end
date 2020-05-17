5c5bc94b6eb952fdc26a2131cd574952a3a9349d
Marwan Bataineh <batainem96@gmail.com>
2020-05-17 14:28:51 -0500

    initial commit

---
+++ src/Tile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Tile() {
+    return (
+        <div className="tile">
+            <img src={"https://user-images.githubusercontent.com/56237004/82157107-20e0f300-9845-11ea-8b31-5bef21a293fe.png"}
+            alt={"Bridget"} className="tile-image" />
+        </div>
+    )
+}
